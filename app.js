55a6b2fa51c18b56423d9c330ac46c87147b6250
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-03-12 14:52:52 +0500

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+let weatherupdate = +prompt("Weathers",)
+// document.write(weatherupdate)
+
+if (weatherupdate >= 50) {
+    // document.write(Garmiha)
+    document.write("heathview")
+
+
+}
+else if (weatherupdate >= 40) {
+    document.write("Garmiha")
+}
+else if (weatherupdate >= 30) {
+    document.write("Kamgarmiha")
+}
+else if (weatherupdate >= 20) {
+    document.write("Normal")
+}
+else if (weatherupdate >= 0) {
+    document.write("Sardiha")
+}
+else {
+    document.write("Barafbariha")
+}
