71c8b3b4843836839d0151e168f95a64cca323a7
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-04-05 23:46:22 +0500

    inputadd

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-let weatherupdate = +prompt("Weathers",)
-// document.write(weatherupdate)
+let form = document.querySelector("#weatherform")
+let forminput = document.querySelector("#weatherinput")
+let htmltext = document.querySelector(".text")
+form.addEventListener("submit", (event) => {
+    let a = (forminput.value)
 
-if (weatherupdate >= 50) {
+    if (a >= 50) {
 
-    document.write("heath view")
+        // show.innerHTML = (value)
+        document.write("heath view")
+    }
+    else if (a >= 40) {
+        document.write("Garmi ha")
+    }
+    else if (a >= 30) {
+        document.write("Kam garmi ha")
+    }
+    else if (a >= 20) {
+        document.write("Normal")
+    }
+    else if (a >= 0) {
+        document.write("Sardi ha")
+    }
+    else {
+        document.write("Barafbariha")
+    }
 
-
-}
-else if (weatherupdate >= 40) {
-    document.write("Garmi ha")
-}
-else if (weatherupdate >= 30) {
-    document.write("Kam garmi ha")
-}
-else if (weatherupdate >= 20) {
-    document.write("Normal")
-}
-else if (weatherupdate >= 0) {
-    document.write("Sardi ha")
-}
-else {
-    document.write("Barafbariha")
-}
+})
