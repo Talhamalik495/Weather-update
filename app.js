872be1fd47c2870de918104da99038fec616977a
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-05-17 02:38:31 +0500

    50% Work Done

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-let form = document.querySelector("#weatherform")
-let forminput = document.querySelector("#weatherinput")
-let htmltext = document.querySelector(".text")
+let form = document.getElementById("weatherform")
+let forminput = document.getElementById("weatherinput")
+let text = document.getElementById("show")
 form.addEventListener("submit", (event) => {
+    // preventDefault("")
     let a = (forminput.value)
 
+    // show.innerHTML = (a)
     if (a >= 50) {
-
-        // show.innerHTML = (value)
-        document.write("heath view")
+        v = "heat view";
+        text.innerHTML = v;
     }
     else if (a >= 40) {
-        document.write("Garmi ha")
+        // document.write("Garmi ha")
+        test = ("Garmi ha")
+        text.innerHTML = test;
     }
     else if (a >= 30) {
         document.write("Kam garmi ha")
